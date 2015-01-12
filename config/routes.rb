94cff1e466bf61377f990a1b782e98e994079aea
bbugh <brainbag@gmail.com>
2015-01-12 15:20:10 -0800

    Installed devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :articles#, only: [:new, :create, :destroy]
   get 'welcome/index'# => 'welcome#index'
 
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
